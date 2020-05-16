1193e9e33da20fffd9f69c31e51daec14ce4d433
hafizhdiaz <hafizhdiaz@gmail.com>
2020-05-16 16:17:23 +0700

    Commit 2

--- index.php
+++ index.php
@@ -98,15 +98,15 @@ if(isset($_POST["masuk"])) {
 
     <div class="intro-section" id="home-section">
       
-      <div class="slide-1" style="background-image: url('assets/images/hero_1.jpg');" data-stellar-background-ratio="0.5">
+      <div class="slide-1" style="background-image: url('assets/images/vespa_1.jpg');" data-stellar-background-ratio="0.5">
         <div class="container">
           <div class="row align-items-center">
             <div class="col-12">
               <div class="row align-items-center">
                 <div class="col-lg-6 mb-4">
-                  <h1  data-aos="fade-up" data-aos-delay="100">Learn From The Expert</h1>
-                  <p class="mb-4"  data-aos="fade-up" data-aos-delay="200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
-                  <p data-aos="fade-up" data-aos-delay="300"><a href="#" class="btn btn-primary py-3 px-5 btn-pill">Admission Now</a></p>
+                  <h1  data-aos="fade-up" data-aos-delay="100">RUSTY MOTORCYCLES</h1>
+                  <p class="mb-4"  data-aos="fade-up" data-aos-delay="200">Cari rekomendasi motormu disini!</p>
+                  <p data-aos="fade-up" data-aos-delay="300"><a href="konsul.php" class="btn btn-primary py-3 px-5 btn-pill">Konsultasikan Sekarang!</a></p>
 
                 </div>
 
